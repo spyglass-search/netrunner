4bba8820eb4936e60329ad1fc9b75e688a11e7ad
Andrew Huynh <a5.t.huynh@gmail.com>
2022-09-09 21:48:02 -0700

    basic cli arg parsing & lens deserialization

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use clap::{Parser, Subcommand};
+use std::path::PathBuf;
+
+mod lib;
+use lib::config::LensConfig;
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+struct Cli {
+    #[clap(short, long, value_parser, value_name = "FILE")]
+    lens_file: PathBuf,
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Crawl,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Crawl => {
+            if let Ok(lens) = LensConfig::from_path(cli.lens_file) {
+                println!("{:?}", lens);
+            }
+        }
+    }
 }
