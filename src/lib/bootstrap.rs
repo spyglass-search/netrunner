6cac461715299b79a91bb7771ae168ba6296ddd4
Andrew Huynh <andrew@spyglass.fyi>
2023-02-11 10:53:41 -0800

    modify sitemap crawling to fetch & process sitemap URLs in parallel

--- src/lib/bootstrap.rs
+++ src/lib/bootstrap.rs
@@ -1,5 +1,6 @@
 use async_recursion::async_recursion;
 use bytes::Buf;
+use dashmap::DashSet;
 use feedfinder::FeedType;
 use flate2::read::GzDecoder;
 use regex::{RegexSet, RegexSetBuilder};
@@ -8,7 +9,7 @@ use rss::Channel;
 use sitemap::reader::{SiteMapEntity, SiteMapReader};
 use spyglass_lens::LensConfig;
 use std::{collections::HashSet, io::Read};
-use texting_robots::Robot;
+use tokio::task::JoinSet;
 use url::Url;
 
 use super::cdx;
@@ -37,8 +38,7 @@ impl Bootstrapper {
 
     pub async fn find_urls(&mut self, lens: &LensConfig) -> anyhow::Result<Vec<String>> {
         // Urls gathered from sitemaps + cdx processing.
-        let mut to_crawl: HashSet<String> = HashSet::new();
-
+        let mut to_crawl: DashSet<String> = DashSet::new();
         let mut cache = CrawlCache::new();
 
         log::info!("Loading lens rules");
@@ -83,7 +83,7 @@ impl Bootstrapper {
         // Third, either read the sitemaps or pull data from a CDX to determine which
         // urls to crawl.
         // ------------------------------------------------------------------------
-        self.process_sitemaps_and_cdx(&mut cache, &mut to_crawl, &allowed, &skipped)
+        self.process_sitemaps_and_cdx(&cache, &mut to_crawl, &allowed, &skipped)
             .await;
 
         // Clear CDX queue after fetching URLs.
@@ -93,7 +93,7 @@ impl Bootstrapper {
         let cleaned: Vec<String> = to_crawl
             .iter()
             .filter_map(|url| {
-                if let Ok(mut url) = Url::parse(url) {
+                if let Ok(mut url) = Url::parse(&url) {
                     url.set_fragment(None);
                     Some(url.to_string())
                 } else {
@@ -106,24 +106,39 @@ impl Bootstrapper {
     }
 
     async fn process_sitemaps_and_cdx(
-        &mut self,
-        cache: &mut CrawlCache,
-        to_crawl: &mut HashSet<String>,
+        &self,
+        cache: &CrawlCache,
+        to_crawl: &mut DashSet<String>,
         allowed: &RegexSet,
         skipped: &RegexSet,
     ) {
         // Crawl sitemaps & rss feeds
+        let mut handles = JoinSet::new();
+        let mut sitemaps = Vec::new();
+
         for info in cache.cache.values().flatten() {
             // Fetch links from RSS feeds
-            to_crawl.extend(self.fetch_rss(info).await);
-
-            // Fetch links from sitemap
+            to_crawl.extend(fetch_rss(info).await);
+            // Grab list of sitemaps
             if let Some(robot) = &info.robot {
                 if !robot.sitemaps.is_empty() {
                     for sitemap in &robot.sitemaps {
-                        to_crawl.extend(self.fetch_sitemap(robot, sitemap, allowed, skipped).await);
+                        sitemaps.push(sitemap.clone());
                     }
                 }
+            }
+        }
+
+        if !sitemaps.is_empty() {
+            log::info!("spawning {} tasks for sitemap fetching", sitemaps.len());
+            for sitemap in sitemaps {
+                let allowed = allowed.clone();
+                let skipped = skipped.clone();
+                handles.spawn(async move { fetch_sitemap(&sitemap, &allowed, &skipped).await });
+            }
+
+            while let Some(Ok(urls)) = handles.join_next().await {
+                to_crawl.extend(urls);
             }
         }
 
@@ -147,6 +162,7 @@ impl Bootstrapper {
 
                 log::info!("found {} urls", filtered.len());
                 to_crawl.extend(filtered);
+
                 if resume.is_none() {
                     break;
                 }
@@ -155,105 +171,115 @@ impl Bootstrapper {
             }
         }
     }
+}
 
-    async fn fetch_rss(&self, info: &SiteInfo) -> Vec<String> {
-        let mut feed_urls: Vec<String> = Vec::new();
+async fn fetch_rss(info: &SiteInfo) -> Vec<String> {
+    let mut feed_urls: Vec<String> = Vec::new();
 
-        for feed in &info.feeds {
-            match feed.feed_type() {
-                FeedType::Atom | FeedType::Rss => {
-                    if let Ok(resp) = reqwest::get(feed.url().to_string()).await {
-                        if let Ok(content) = resp.bytes().await {
-                            if let Ok(channel) = Channel::read_from(&content[..]) {
-                                for item in channel.items {
-                                    if let Some(link) = item.link {
-                                        feed_urls.push(link);
-                                    }
+    for feed in &info.feeds {
+        match feed.feed_type() {
+            FeedType::Atom | FeedType::Rss => {
+                if let Ok(resp) = reqwest::get(feed.url().to_string()).await {
+                    if let Ok(content) = resp.bytes().await {
+                        if let Ok(channel) = Channel::read_from(&content[..]) {
+                            for item in channel.items {
+                                if let Some(link) = item.link {
+                                    feed_urls.push(link);
                                 }
                             }
                         }
                     }
                 }
-                _ => {}
             }
+            _ => {}
         }
-
-        feed_urls
     }
 
-    /// Fetch and parse a sitemap file
-    #[async_recursion]
-    async fn fetch_sitemap(
-        &self,
-        robot: &Robot,
-        sitemap_url: &str,
-        allowed: &RegexSet,
-        skipped: &RegexSet,
-    ) -> Vec<String> {
-        log::debug!("fetching sitemap: {}", sitemap_url);
-        let mut urls: Vec<String> = Vec::new();
+    feed_urls
+}
 
-        if let Ok(resp) = self.client.get(sitemap_url).send().await {
-            if resp.status().is_success() {
-                let mut buf = String::new();
-                // Decode gzipped files. Doesn't work automatically if they were
-                // gzipped before uploading it to their destination.
-                if sitemap_url.ends_with(".gz") {
-                    if let Ok(text) = resp.bytes().await {
-                        let mut decoder = GzDecoder::new(text.reader());
-                        decoder.read_to_string(&mut buf).unwrap();
+/// Fetch and parse a sitemap file
+#[async_recursion]
+async fn fetch_sitemap(
+    sitemap_url: &str,
+    allowed: &RegexSet,
+    skipped: &RegexSet,
+) -> HashSet<String> {
+    log::debug!("fetching sitemap: {}", sitemap_url);
+
+    let mut urls: HashSet<String> = HashSet::new();
+    let client = http_client();
+
+    if let Ok(resp) = client.get(sitemap_url).send().await {
+        if resp.status().is_success() {
+            let mut buf = String::new();
+            // Decode gzipped files. Doesn't work automatically if they were
+            // gzipped before uploading it to their destination.
+            if sitemap_url.ends_with(".gz") {
+                if let Ok(text) = resp.bytes().await {
+                    let mut decoder = GzDecoder::new(text.reader());
+                    decoder.read_to_string(&mut buf).unwrap();
+                }
+            } else if let Ok(text) = resp.text().await {
+                buf = text.replace('\u{feff}', "");
+            }
+
+            let parser = SiteMapReader::new(buf.as_bytes());
+            let mut sitemaps = Vec::new();
+            for entity in parser {
+                match entity {
+                    SiteMapEntity::Url(url_entry) => {
+                        if let Some(loc) = url_entry.loc.get_url() {
+                            let url = loc.to_string();
+                            if allowed.is_match(&url) && !skipped.is_match(&url) {
+                                urls.insert(url);
+                            }
+                        }
                     }
-                } else if let Ok(text) = resp.text().await {
-                    buf = text.replace('\u{feff}', "");
+                    SiteMapEntity::SiteMap(sitemap_entry) => {
+                        if let Some(loc) = sitemap_entry.loc.get_url() {
+                            sitemaps.push(loc.to_string());
+                        }
+                    }
+                    _ => {}
+                }
+            }
+
+            if !sitemaps.is_empty() {
+                let mut set = JoinSet::new();
+                log::info!("spawning {} tasks for sitemap fetching", sitemaps.len());
+                for sitemap in sitemaps {
+                    let allowed = allowed.clone();
+                    let skipped = skipped.clone();
+                    set.spawn(async move { fetch_sitemap(&sitemap, &allowed, &skipped).await });
                 }
 
-                let parser = SiteMapReader::new(buf.as_bytes());
-                for entity in parser {
-                    match entity {
-                        SiteMapEntity::Url(url_entry) => {
-                            if let Some(loc) = url_entry.loc.get_url() {
-                                let url = loc.to_string();
-                                if robot.allowed(&url)
-                                    && allowed.is_match(&url)
-                                    && !skipped.is_match(&url)
-                                {
-                                    urls.push(url);
-                                }
-                            }
-                        }
-                        SiteMapEntity::SiteMap(sitemap_entry) => {
-                            if let Some(loc) = sitemap_entry.loc.get_url() {
-                                urls.extend(
-                                    self.fetch_sitemap(robot, loc.as_str(), allowed, skipped)
-                                        .await,
-                                );
-                            }
-                        }
-                        _ => {}
-                    }
+                while let Some(Ok(found)) = set.join_next().await {
+                    urls.extend(found);
                 }
             }
         }
+    }
 
+    if !urls.is_empty() {
         log::info!("found {} urls for {}", urls.len(), sitemap_url);
-        urls
     }
+
+    urls
 }
 
 #[cfg(test)]
 mod test {
-    use crate::{bootstrap::Bootstrapper, crawler::http_client, site::SiteInfo};
+    use crate::{bootstrap::fetch_rss, site::SiteInfo};
 
     #[tokio::test]
     #[ignore = "only used for dev"]
     async fn test_fetch_rss() {
-        let bs = Bootstrapper::new(&http_client());
-
         let info = SiteInfo::new("atp.fm")
             .await
             .expect("unable to create siteinfo");
 
-        let feed_urls = bs.fetch_rss(&info).await;
+        let feed_urls = fetch_rss(&info).await;
         assert_eq!(feed_urls.len(), 515);
     }
 }
